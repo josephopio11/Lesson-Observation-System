fd099973fe01d79b5344a3dab02447ee9ad40962
Joseph Opio <josephopio11@gmail.com>
2021-10-07 12:27:16 +0200

    Added other classes

--- resources/views/lessons/create.blade.php
+++ resources/views/lessons/create.blade.php
@@ -42,6 +42,12 @@
                                             <option value="9">Year 9</option>
                                             <option value="8">Year 8</option>
                                             <option value="7">Year 7</option>
+                                            <option value="6">Year 9</option>
+                                            <option value="5">Year 8</option>
+                                            <option value="4">Year 7</option>
+                                            <option value="3">Year 9</option>
+                                            <option value="2">Year 8</option>
+                                            <option value="1">Year 7</option>
                                         </select>
                                     </div>
                                 </fieldset>
@@ -53,96 +59,96 @@
                                             <label for="">1. Objectives were communicated clearly at the start of the lesson
                                             </label>
                                             <input name="objcommnclearly" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('objcommnclearly')">
                                         </div>
                                         <div
                                             class="form-group list-group-item list-group-item-action list-group-item-primary">
                                             <label for="">2. The learning needs of all levels of students were incorporated
                                                 in the teacher's planning</label>
                                             <input name="inclusion" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('inclusion')">
                                         </div>
                                         <div
                                             class="form-group list-group-item list-group-item-action list-group-item-secondary">
                                             <label for="">3. Lesson was reviewed at the end</label>
                                             <input name="reviewed" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('reviewed')">
                                         </div>
                                         <div
                                             class="form-group list-group-item list-group-item-action list-group-item-success">
                                             <label for="">4. Teacher had thorough knowledge of the subject content</label>
                                             <input name="thrknow" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('thrknow')">
                                         </div>
                                         <div
                                             class="form-group list-group-item list-group-item-action list-group-item-danger">
                                             <label for="">5. Subject material was appropriate for the lesson</label>
                                             <input name="subjmat" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('subjmat')">
                                         </div>
                                         <div
                                             class="form-group list-group-item list-group-item-action list-group-item-warning">
                                             <label for="">6. Knowledge was made relevant and interesting for the
                                                 learners</label>
                                             <input name="knowrel" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('knowrel')">
                                         </div>
                                         <div class="form-group list-group-item list-group-item-action list-group-item-info">
                                             <label for="">7. The ideas and experiences of students were drawn upon</label>
                                             <input name="ideaexp" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('ideaexp')">
                                         </div>
                                         <div class="form-group list-group-item list-group-item-action list-group-item-dark">
                                             <label for="">8. A variety of activities and questioning techniques were
                                                 used</label>
                                             <input name="actandqn" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('actandqn')">
                                         </div>
                                         <div
                                             class="form-group list-group-item list-group-item-action list-group-item-success">
                                             <label for="">9. Students were praised regularly for their good effort and
                                                 achievement</label>
                                             <input name="praise" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('praise')">
                                         </div>
                                         <div
                                             class="form-group list-group-item list-group-item-action list-group-item-warning">
                                             <label for="">10. Prompt action was taken to address poor behaviour</label>
                                             <input name="poorbehave" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('poorbehave')">
                                         </div>
                                         <div
                                             class="form-group list-group-item list-group-item-action list-group-item-danger">
                                             <label for="">11. Students were treated fairly with equal emphasis on work for
                                                 boys and girls of all abilities</label>
                                             <input name="fairness" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('fairness')">
                                         </div>
                                         <div class="form-group form-group list-group-item list-group-item-action">
                                             <label for="">12. Mistakes/misconceptions were recognised and used
                                                 constructively to facilitate learning</label>
                                             <input name="recmiscon" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('recmiscon')">
                                         </div>
                                         <div
                                             class="form-group form-group list-group-item list-group-item-action list-group-item-primary">
                                             <label for="">13. Students remained fully engaged throughout the lesson and
                                                 their understanding was assessed by using the teacher's questions</label>
                                             <input name="studengaged" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('studengaged')">
                                         </div>
                                         <div
                                             class="form-group form-group list-group-item list-group-item-action list-group-item-secondary">
                                             <label for="">14. Time was well utilised and learning was maintained for the
                                                 full time available</label>
                                             <input name="timeutil" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('timeutil')">
                                         </div>
                                         <div
                                             class="form-group form-group list-group-item list-group-item-action list-group-item-warning">
                                             <label for="">15. A good pace was maintained throughout the lesson</label>
                                             <input name="goodprac" type="range" class="form-control-range"
-                                                id="formControlRange" min="0" max="10" value="old('name')">
+                                                id="formControlRange" min="0" max="10" value="old('goodprac')">
                                         </div>
                                     </div>
 
@@ -157,7 +163,7 @@
                                             <div class="float-right">Excellent</div>
                                         </div>
                                         <input name="descriptor" type="range" class="form-control-range"
-                                            id="formControlRange" min="1" max="5" value="old('name')">
+                                            id="formControlRange" min="1" max="5" value="old('descriptor')">
                                     </div>
                                     <div class="form-group">
                                         <label for="editor">Observer Comment</label>
@@ -167,26 +173,26 @@
                                 </fieldset>
                                 
                                 {{-- id 
-name
-class
-objcommnclearly
-inclusion
-reviewed
-thrknow
-subjmat
-knowrel
-ideaexp
-actandqn
-praise
-poorbehave
-fairness
-recmiscon
-studengaged
-timeutil
-goodprac
-descriptor
-comment
-user_id --}}
+                                    name
+                                    class
+                                    objcommnclearly
+                                    inclusion
+                                    reviewed
+                                    thrknow
+                                    subjmat
+                                    knowrel
+                                    ideaexp
+                                    actandqn
+                                    praise
+                                    poorbehave
+                                    fairness
+                                    recmiscon
+                                    studengaged
+                                    timeutil
+                                    goodprac
+                                    descriptor
+                                    comment
+                                    user_id --}}
 
 
 
